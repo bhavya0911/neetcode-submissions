faef1c73a031f8e26dbe1d1508f651e5d08ad26a
bhavya0911 <48725647+bhavya0911@users.noreply.github.com>
2026-06-26 13:22:49 +0530

    Add: lru-cache - submission-5

---
+++ Data Structures & Algorithms/lru-cache/submission-5.py
@@ -0,0 +1,23 @@
+class LRUCache:
+    
+    def __init__(self, capacity: int):
+        self.cache = dict()
+        self.used = []
+        self.cap = capacity
+
+    def get(self, key: int) -> int:
+        if key in self.used:
+            self.used.remove(key)
+        if key in self.cache:
+            self.used.append(key)
+        return self.cache.get(key, -1)
+
+    def put(self, key: int, value: int) -> None:
+        if not (key in self.cache) and (len(self.cache) + 1) > self.cap:
+            poper = self.used.pop(0)
+            print(self.cache.pop(poper, None))
+
+        self.cache[key] = value
+        if key in self.used:
+            self.used.remove(key)
+        self.used.append(key)
